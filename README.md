895c2bf4d9df11244345395d168065039b2c22b7
AllenJoeG <allen.joeg@gmail.com>
2023-09-26 16:09:22 -0500

    terraform extension brought in, variables.tf created, terraform init and validated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fem-eci-terraform-tfe
 
+## shell commands
+terraform init
+terraform validate (lints it, always do before push)
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "organization_name" {
+  default = "FEM-CloudInfra"
+  type    = string
+}
+
+variable "organization_usernames" {
+  default = ["AllenJoeG"]
+  type    = list(string)
+}
