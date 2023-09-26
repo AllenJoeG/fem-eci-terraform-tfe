d18a8fcee28838b8045dfc98ea968a313f32e2e2
AllenJoeG <allen.joeg@gmail.com>
2023-09-26 16:03:38 -0500

    main.tf initialized and some notes

---
+++ main.tf
@@ -0,0 +1,29 @@
+module "team" {
+  source = "ALT-F4-LLC/team/tfe"
+  version = "0.2.0"
+
+  name = "fem-eci-team"
+  organization_name = var.organization_name
+  usernames = var.organization_usernames
+}
+
+module "project" {
+  source = "ALT-F4-LLC/project/tfe"
+  version = "0.4.0"
+
+  description = "Following FEM Enterprise Cloud Infrastructure course"
+  name = "fem-eci-project"
+  organization_name = var.organization_name
+  team_ids = [module.team.id]
+}
+
+module "workspace" {
+  source = "ALT-F4-LLC/workspace/tfe"
+  version = "0.6.0"
+
+  description = "Still following FEM Enterprise Cloud Infrastructure"
+  execution_mode = "local"
+  name = "fem-eci-workspace"
+  organization_name = var.organization_name
+  project_id = module.project.id
+}
---
+++ notes.md
@@ -0,0 +1,6 @@
+## main.tf
+  source can point to local files, module names, or even ssh to stuff
+  
+  module support is there right out of the box. In Terraform browser > Registry you can set up private modules.
+
+  Terraform has an automated dependency tree generator. Anything listed here, it will know how to sort the order of the dependencies in order. 
